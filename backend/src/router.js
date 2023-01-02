fde1ff7cee1b4175a556477ac11c8f0aba274ba5
curiouseagle <anais.maligebordes@gmail.com>
2023-01-02 11:25:55 +0100

    quest express 9.1 done

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
@@ -7,6 +12,19 @@ const itemControllers = require("./controllers/itemControllers");
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
+
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
